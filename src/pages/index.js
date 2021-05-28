179cbd0530a571740400227998b5bc4bb9e6a213
Alauddin Al Azad <saimon.azad@hotmail.com>
2021-05-28 18:00:04 +0600

    Design system : Material UI added

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Typography from "@material-ui/core/Typography";
+
+export default function Home() {
+  return (
+    <div>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Next.js example
+      </Typography>
+    </div>
+  );
+}
