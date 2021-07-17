0ab17c293543168b2a5cf86f0b23fa9302aa990b
Alauddin Al Azad <saimon.azad@hotmail.com>
2021-07-17 07:17:27 +0600

    following ui added

--- src/components/profile/posts/following/index.js
+++ src/components/profile/posts/following/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { Avatar, Box, makeStyles, Typography } from "@material-ui/core";
+import CmtAvatarGroup from "../../../../../@coremat/CmtAvatarGroup";
+import CmtAvatar from "../../../../../@coremat/CmtAvatar";
 
+import useSWR from "swr";
+import { fetcher } from "../../../../services/fetcher";
+import { useTheme } from "@material-ui/core/styles";
+import { fetchFollowingUrl } from "../../../../services/follow";
 const useStyles = makeStyles((theme) => ({
   root: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(1.3),
     color: theme.palette.text.mineShaft,
   },
   follower: {
@@ -16,10 +22,66 @@ const useStyles = makeStyles((theme) => ({
       marginLeft: theme.spacing(1),
     },
   },
+  titleRoot: {
+    color: theme.palette.common.white,
+    fontSize: 14,
+  },
+  avatarRoot: {
+    marginTop: 2,
+    marginBottom: 2,
+  },
+  avatar: {
+    "@media screen and (min-width: 1280px) and (max-width: 1368px)": {
+      width: 30,
+      height: 36,
+    },
+    [theme.breakpoints.up("md")]: {
+      width: 40,
+      height: 40,
+    },
+    [theme.breakpoints.down("md")]: {
+      width: 38,
+      height: 38,
+    },
+    [theme.breakpoints.up("xl")]: {
+      width: 56,
+      height: 56,
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: 40,
+      height: 40,
+    },
+  },
 }));
 const Following = () => {
-  const classes = useStyles();
+  const theme = useTheme();
 
+  const classes = useStyles();
+  const { data, error } = useSWR(fetchFollowingUrl, fetcher);
+  let following = data;
+
+  const moreItemsTooltip = (data) => (
+    <CmtList
+      data={data}
+      renderRow={(item, index) => {
+        return (
+          <CmtObjectSummary
+            key={index}
+            avatar={
+              <CmtAvatar
+                className={classes.avatarRoot}
+                size={20}
+                // src={item.profilePic}
+                alt={item.followed}
+              />
+            }
+            title={item.followed}
+            titleProps={{ className: classes.titleRoot }}
+          />
+        );
+      }}
+    />
+  );
   return (
     <Box
       bgcolor="background.box"
@@ -33,13 +95,28 @@ const Following = () => {
       <Box>
         <Typography>Following</Typography>
       </Box>
-      <Box display="flex" alignItems="center" className={classes.follower}>
-        <Avatar src="https://i.pravatar.cc/50?img=1" />
-        <Avatar src="https://i.pravatar.cc/50?img=2" />
-        <Avatar src="https://i.pravatar.cc/50?img=3" />
-        <Avatar src="https://i.pravatar.cc/50?img=4" />
-        <Typography>+7</Typography>
-      </Box>
+      <CmtAvatarGroup
+        classes={{ avatar: classes.avatar }}
+        items={following}
+        srcKey="profilePic"
+        spacing={1}
+        max={6}
+        size="small"
+        titleKey="name"
+        renderItemSummary={(item) => (
+          <React.Fragment>
+            <Typography color="inherit">{item.followed}</Typography>
+            <p className={"pb-2"}>{"It's very engaging. Right?"}</p>
+            <CmtAvatar
+              // src={item.profilePic}
+              alt={item.followed}
+              variant="rounded"
+              size={125}
+            />
+          </React.Fragment>
+        )}
+        renderMore={(restItems) => moreItemsTooltip(restItems)}
+      />
     </Box>
   );
 };
