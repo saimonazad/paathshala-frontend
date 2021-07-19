d5bf568fb7300b001b7074988ef8a84f931d6499
Alauddin Al Azad <saimon.azad@hotmail.com>
2021-07-19 07:53:20 +0600

    next js dev and prod env added

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
-module.exports = {
-  env: {
-    BACKEND_URL: "http://localhost:8000",
-  },
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      /* development only config options here */
+      env: {
+        BACKEND_URL: "http://localhost:8000",
+      },
+    };
+  }
+
+  return {
+    /* config options for all phases except development here */
+    env: {
+      BACKEND_URL: "https://paathshala.staging.baeinnovations.com",
+    },
+  };
 };
